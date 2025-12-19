28dde681fd762e13a34532b63fc283b196dc6815
naziko <gulnazikmahamadalieva@gmail.com>
2025-12-19 22:50:11 +0600

    control4

--- CineBoard/urls.py
+++ CineBoard/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from .import views
+from . import views
 
-app_name='cineboard'
+app_name = 'cineboard'
 
 urlpatterns = [
-    path('register_cine/', views.RegisterView.as_view(), name='register_cine'),
-    path('login_cine/', views.AuthLoginView.as_view(), name='login_cine'),
-    path('logout_cine/', views.AuthLogoutView.as_view(), name='logout_cine'),
-    path('all_films/', views.AllFilmsListView.as_view(), name='all_films'),
-    path('all_films/<int:id>/update/', views.UpdateFilmView.as_view(), name='update'),
-    path('all_films/<int:id>/delete/', views.DeleteFilmView.as_view(), name='delete'),
-    path('create_film/', views.CreateFilmView.as_view(), name='create_film')
+    # AUTH
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
+    # FILMS
+    path('', views.AllFilmsListView.as_view(), name='all_films'),
+    path('film/create/', views.CreateFilmView.as_view(), name='create_film'),
+    path('film/<int:id>/edit/', views.UpdateFilmView.as_view(), name='update_film'),
+    path('film/<int:id>/delete/', views.delete_film, name='delete_film'),
 ]
--- CineBoard/views.py
+++ CineBoard/views.py
@@ -1,95 +1,97 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.views import generic
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from . import models, forms
-from django.http import HttpResponse
+from .models import Films
+from .forms import FilmsForm
 
 
-
-
-class CreateFilmView(generic.CreateView):
-    model = models.Films
-    form_class = forms.FilmsForm
-    template_name = 'cineboard/pages/movie_add_page.html'
-    success_url = '/all_films/'
-
-
-class UpdateFilmView(generic.UpdateView):
-    model = models.Films
-    form_class = forms.FilmsForm
-    template_name = 'cineboard/pages/movie_edit_page.html'
-    success_url = '/all_films/'
-
-    def get_object(self, *args, **kwargs):
-        film_id = self.kwargs.get('id')
-        return get_object_or_404(models.Films, id=film_id)
-
-
-class DeleteFilmView(generic.DeleteView):
-    template_name = 'cineboard/pages/movie_remove_page.html'
-    success_url = '/all_films/'
-
-    def get_object(self, *args, **kwargs):
-        film_id = self.kwargs.get('id')
-        return get_object_or_404(models.Films, id=film_id)
-
+# FILMS
 
 class AllFilmsListView(LoginRequiredMixin, generic.ListView):
-    model = models.Films
-    template_name = 'cineboard/pages/movies_overview.html'
-    context_object_name = 'tv_lst'
+    model = Films
+    template_name = 'cineboard/list_page.html'
+    context_object_name = 'films'
 
 
-# ================== AUTH ==================
+class CreateFilmView(LoginRequiredMixin, generic.CreateView):
+    model = Films
+    form_class = FilmsForm
+    template_name = 'cineboard/form_page.html'
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавить фильм'
+        return context
+
+
+class UpdateFilmView(LoginRequiredMixin, generic.UpdateView):
+    model = Films
+    form_class = FilmsForm
+    template_name = 'cineboard/form_page.html'
+    success_url = '/'
+
+    def get_object(self):
+        return get_object_or_404(Films, id=self.kwargs['id'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Редактировать фильм'
+        return context
+
+
+def delete_film(request, id):
+    film = get_object_or_404(Films, id=id)
+    if request.method == 'POST':
+        film.delete()
+        return redirect('/')
+    return render(request, 'cineboard/form_page.html', {
+        'title': 'Удалить фильм',
+        'form': None
+    })
+
+
+# AUTH
 
 class RegisterView(generic.View):
     def get(self, request):
-        form = UserCreationForm()
-        return render(
-            request,
-            'cineboard/users/signup_page.html',
-            {'form': form}
-        )
+        return render(request, 'cineboard/form_page.html', {
+            'form': UserCreationForm(),
+            'title': 'Регистрация'
+        })
 
     def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('cineboard:login')
-        return render(
-            request,
-            'cineboard/users/signup_page.html',
-            {'form': form}
-        )
+            return redirect('/login/')
+        return render(request, 'cineboard/form_page.html', {
+            'form': form,
+            'title': 'Регистрация'
+        })
 
 
-class AuthLoginView(generic.View):
+class LoginView(generic.View):
     def get(self, request):
-        form = AuthenticationForm()
-        return render(
-            request,
-            'cineboard/users/signin_page.html',
-            {'form': form}
-        )
+        return render(request, 'cineboard/form_page.html', {
+            'form': AuthenticationForm(),
+            'title': 'Вход'
+        })
 
     def post(self, request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect('cineboard:all_films')
-        return render(
-            request,
-            'cineboard/users/signin_page.html',
-            {'form': form }
-        )
+            return redirect('/')
+        return render(request, 'cineboard/form_page.html', {
+            'form': form,
+            'title': 'Вход'
+        })
 
 
-class AuthLogoutView(generic.View):
-    def get(self, request):
-        logout(request)
-        return redirect('cineboard:login')
+def logout_view(request):
+    logout(request)
+    return redirect('/login/')
